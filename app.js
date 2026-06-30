ba6593a4e5ce6f8b8364f90b998b160dec5d392f
Emilien HERGOTT <emilien.hergott@mewo-campus.fr>
2026-06-30 13:20:05 +0200

    install dotenv

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.API_PORT
+
+console.log(process.env);
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' })
-})
+app.get('/health', (req, res) => [
+    res.status(200).json({
+        "status": "ok"
+    })
+]);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
